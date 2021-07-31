c22b3377a6a93bab3bd9b447c2be4def0f1608dd
ShinJustinHolly3317 <slammingthebasket123@gmail.com>
2021-07-31 16:00:58 +0800

    feat: update assignment-4 CSS

---
+++ Week-1/Assignment-4/js/home-listeners.js
@@ -0,0 +1,18 @@
+/* elements */
+const burgerBtn = document.querySelector('.js-burger-button')
+const collapsingNavbar = document.querySelector('.js-collapse-navbar')
+const collapseClosingBtn = document.querySelector('.js-burger-close-btn')
+const siteMask = document.querySelector('.site-mask')
+
+
+/* Listeners */
+burgerBtn.addEventListener('click', () => {
+  console.log('hahahaha');
+  collapsingNavbar.classList.toggle('floating-inside-window')
+  siteMask.classList.toggle('js-mask-fadein')
+})
+
+collapseClosingBtn.addEventListener('click', () => {
+  collapsingNavbar.classList.toggle('floating-inside-window')
+  siteMask.classList.toggle('js-mask-fadein')
+})
