4a34b7f4d4a40a6323e825f491c561845c8edb75
ShinJustinHolly3317 <slammingthebasket123@gmail.com>
2021-08-07 19:49:14 +0800

    finished assignment-5

---
+++ Week-2/Assignment-5/assignmet-5.js
@@ -0,0 +1,34 @@
+// binary search
+function binarySearchPosition(numbers, target) {
+  /* 
+  1. Array needs to be sorted in accending order.
+  2. Cut the array in half, compare that the Target is bigger or smaller than the mid element，then find out which half the Targe is in.
+  3. Pick the half array which the Target is in.
+  4. Keep cutting in half until there is only one element in the array
+  5. Find out the element equals to Target or not
+  */
+  let beginIndex = 0
+  let lastIndex = numbers.length - 1
+
+  while (lastIndex >= beginIndex) {
+    const midIndex = Math.floor((beginIndex + lastIndex )/ 2)
+    const midNum = numbers[midIndex]
+    if (midNum === target) {
+      // base case
+      return midIndex
+    } else if (midNum > target) {
+      // slice the array to the left part
+      lastIndex = midIndex - 1
+    } else {
+      beginIndex = midIndex + 1
+    }
+  }
+  
+  // Target not in this array
+  return -1
+}
+
+
+console.log( binarySearchPosition([1, 2, 5, 6, 7], 1) ); // should print 0
+console.log( binarySearchPosition([1, 2, 5, 6, 7], 6) ); // should print 3
+
